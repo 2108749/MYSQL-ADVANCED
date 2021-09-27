48aa71eeaad1fc582ddf4a9b327b97a54d2439e3
Zorro <diegoblom01@gmail.com>
2021-09-27 10:07:58 +0200

    opdracht

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) VALUES (201002, "Rick", "Bro", "0C", "Amstelveen", "1822NK", "Amsterdam" 12-12-2012); 
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) VALUES (27182, "Sem", "Mees", "0A", "Van der specht 0", "8888NK", "Amsterdam" 12-12-2012);
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) VALUES (5, "Hayder", "Habiba", "0C", "Amsterdamse weg 8", "1156AC", 2012-12-12, "Amsterdam")
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) VALUES (6, "Jelmer", "Westside", "0C", "Amsterdamse weg 21", "1156BC", 2020-12-12, "Amstelveen")
